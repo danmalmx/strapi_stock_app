b7f15681ce165a3eb291cfa67eb114b9ec7fe27f
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-03-21 21:25:44 +0100

    Add AddStockEvent component

---
+++ stock_app_frontend/src/components/AddStockEvent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class  AddStockEvent extends Component {
+  render(){
+
+    return (
+      <div className="AddStockEvent">
+        Add Stock Event
+      </div>
+    )
+  }
+}
+
+export default AddStockEvent
